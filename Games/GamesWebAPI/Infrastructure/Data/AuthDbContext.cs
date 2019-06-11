a4651b9e3ab7dfa320b3ec2f7677980cc8e92756
dvbreva <jump.the.stars@abv.bg>
2019-06-11 03:52:35 +0300

    implemented authentication with bearer token

---
+++ Games/GamesWebAPI/Infrastructure/Data/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GamesWebAPI.Infrastructure.Data
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthDbContext()
+            : base("AuthDbContext")
+        {
+
+        }
+    }
+}
