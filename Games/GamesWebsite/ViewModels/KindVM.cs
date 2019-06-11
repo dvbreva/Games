75b8d5f1ad309a3d88f8766829e4bafc009ad03c
dvbreva <jump.the.stars@abv.bg>
2019-06-11 21:35:26 +0300

    added VMs

---
+++ Games/GamesWebsite/ViewModels/KindVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GamesWebsite.ViewModels
+{
+    public class KindVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public bool Suitability { get; set; }
+
+    }
+}
